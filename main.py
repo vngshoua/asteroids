bf63ebb8a8d6322f1708a08e8d114a6e422d4772
vngshoua <61334160+vngshoua@users.noreply.github.com>
2024-10-20 22:57:08 -0700

    game loop update

--- main.py
+++ main.py
@@ -2,10 +2,22 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    alive = True
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+    while alive:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color='black')
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
