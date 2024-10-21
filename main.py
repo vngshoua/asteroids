aa52aa0a19dabcf14ea82e5d5912d29f13a717c5
vngshoua <61334160+vngshoua@users.noreply.github.com>
2024-10-20 22:25:12 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# this allow us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+def main():
+    print("Starting asteroids!")
+
+
+if __name__ == "__main__":
+    main()
